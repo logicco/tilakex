7c33b949a0b42c643a976aa7ae376ab555e1fe9d
unknown <logicco.io>
2021-07-23 18:01:43 -0500

    #1 - fixed axios base_url for production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = 'http://api.tilakex.test';
+axios.defaults.baseURL = 'https://api.tilakex.com';
 window.axios.defaults.withCredentials = true;
 
 /**
